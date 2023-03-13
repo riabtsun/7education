51564b6c8b77fc6ce5f5f74c05284a8aa5dfde3a
dryabtsun <riabtsun@i.ua>
2023-03-13 12:19:35 +0200

    1 js homework

---
+++ JS/1/script.js
@@ -0,0 +1,16 @@
+// const name = prompt("enter your name");
+// const age = prompt("enter your age");
+// const city = prompt("enter your city");
+
+// document.write(`Your name is ${name}, ${age} years old from ${city}`);
+
+document.write(
+  '<div style="margin: 50px auto; width: fit-content">\n' +
+    '    <header style="width: 800px; height: 200px; display: block">header 800*200</header>\n' +
+    '    <div style="display: flex">\n' +
+    '        <nav style="width: 100px; height: 400px; display: block">nav</nav>\n' +
+    '        <section style="width: 700px; height: 400px; display: block">700*400</section>\n' +
+    "    </div>\n" +
+    '    <footer style="width: 800px; height: 200px">footer</footer>\n' +
+    "</div>"
+);
