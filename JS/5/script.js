9a0fcc42d996651c3d15be2ff65b513d3f10d1d1
dryabtsun <riabtsun@i.ua>
2023-04-19 14:06:19 +0300

    6 hw js

--- JS/5/script.js
+++ JS/5/script.js
@@ -7,57 +7,56 @@
 //   Нехай цей метод роздруковує food і location тварини, що прийшла на прийом.
 //   У методі main створіть масив типу Animal, в який запишіть тварин всіх типів, що є у вас. У циклі надсилайте їх на прийом до ветеринара.
 
-function Animal(name, food, location) {
+class Animal {
+  constructor(name, food, location) {
     this.food = 'Kovbasa';
     this.location = 'Earth';
     this.name = name;
+  }
 }
 
 Animal.prototype.makeNoise = function () {
-    console.log(` ${this.name} Така тварина спить`);
+  console.log(` ${this.name} Така тварина спить`);
 };
 Animal.prototype.eat = function () {
-    console.log(`${this.name}Така тварина їсть`);
+  console.log(`${this.name}Така тварина їсть`);
 };
 Animal.prototype.sleep = function () {
-    console.log(`тварина спить`);
+  console.log(`тварина спить`);
 };
 
 function Dog(isHunter) {
-    this.isHunter = true;
-    this.makeNoise = (name) => {
-        console.log(` ${name} Така тварина не спить`);
-    };
+  this.isHunter = true;
+  this.makeNoise = (name) => {
+    console.log(` ${name} Така тварина не спить`);
+  };
 }
 
 function Cat(isHunter) {
-    this.isHunter = true;
+  this.isHunter = true;
 }
 
 function Horse(isHunter) {
-    this.isHunter = true;
+  this.isHunter = true;
 }
 
 Dog.prototype = new Animal();
 Dog.prototype.makeNoise = function () {
-    console.log(` ${this.name} Така тварина спить 10 годин`)
-}
+  console.log(` ${this.name} Така тварина спить 10 годин`);
+};
 
 Cat.prototype = new Animal();
 Horse.prototype = new Animal();
 
-function Veterinarian() {
-
-}
+function Veterinarian() {}
 
 Veterinarian.prototype.treatAnimal = function (animal) {
-    console.log(Object.values(animal));
+  console.log(Object.values(animal));
 };
 
 function main() {
-    const animals = [new Dog(), new Cat(), new Horse()]
-    animals.forEach(el => {
-       new Veterinarian().treatAnimal(el)
-    })
+  const animals = [new Dog(), new Cat(), new Horse()];
+  animals.forEach((el) => {
+    new Veterinarian().treatAnimal(el);
+  });
 }
-
--- JS/6/script.js
+++ JS/6/script.js
@@ -1,12 +1,68 @@
 class CreateNewUser {
-    constructor(name, surname) {
-        this.name = name
-        this.surname = surname
-    }
+  constructor(name, surname, birthday) {
+    this.name = name;
+    this.surname = surname;
+    this.birthday = birthday;
+  }
 
-    getLogin() {
-        (`${this.name[0]}${this.surname}`).toLowerCase()
-    }
+  getLogin() {
+    `${this.name[0]}${this.surname}`.toLowerCase();
+  }
+  getAge() {
+    const date = new Date().getFullYear();
+    const userBirthYear = this.birthday.split('.')[2];
+    const age = date - userBirthYear;
+    console.log(`User has ${age} years`);
+  }
+  getPassword() {
+    console.log(
+      this.name[0].toUpperCase() +
+        this.surname.toLowerCase() +
+        this.birthday.split('.')[2]
+    );
+  }
 }
 
-const newUser = new CreateNewUser(prompt('Enter your name'), prompt('Enter your surname'))
+// const newUser = new CreateNewUser(
+//   prompt('Enter your name'),
+//   prompt('Enter your surname'),
+//   prompt('Enter your birthday (dd.mm.yyyy)')
+// );
+
+const url = 'https://fakestoreapi.com/products';
+
+class Card {
+  constructor(name, img, price, description) {
+    this.name = name;
+    this.img = img;
+    this.productPrice = price;
+    this.productDescription = description;
+  }
+}
+let products = [];
+
+async function createCards() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.log(data);
+
+  data.forEach((card) => {
+    products.push(
+      new Card(card.title, card.image, card.productPrice, card.description)
+    );
+  });
+}
+createCards();
+console.log(products);
+
+products.forEach((card) => {
+  document.write(`
+              <div class="card" style="width: 18rem;">
+                <img src="${card.img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${card.name}</h5>
+                  <p class="card-text">${card.productDescription}.</p>
+                  <a href="#" class="btn btn-primary">${card.productPrice}</a>
+                </div>
+              </div>`);
+});
