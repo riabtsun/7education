5d99c2085d1d4ace31057168f89d98cc7ea565ed
dryabtsun <riabtsun@i.ua>
2023-05-02 09:56:00 +0300

    resolved 5 & 4 tasks

--- JS/7/script.js
+++ JS/7/script.js
@@ -22,3 +22,25 @@ blockGenerator.addEventListener('click', () => {
   thirdTask.append(newBlock);
   inputGenerator.value = '';
 });
+
+window.onload = () => {
+  const changeImgBtn = document.querySelector('.changeImg');
+  const resetImg = document.querySelector('.resetImage');
+  resetImg.style.height = '300px';
+  resetImg.src = 'https://itproger.com/img/courses/1476977240.jpg';
+  let counter = 0;
+  changeImgBtn.addEventListener('click', () => {
+    counter += 1;
+    resetImg.src = `https://itproger.com/img/courses/1476977488.jpg`;
+    counter === 1
+      ? (resetImg.src =
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png')
+      : 'https://itproger.com/img/courses/1476977240.jpg';
+  });
+};
+
+const paragraphsBlock = document.querySelector('.fifthTask');
+paragraphsBlock.addEventListener('click', (event) => {
+  let paragraph = event.target;
+  paragraph.remove();
+});
