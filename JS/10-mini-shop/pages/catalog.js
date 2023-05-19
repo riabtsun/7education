3b0740f34e231f0326ee70df12bd3299ee852036
dryabtsun <riabtsun@i.ua>
2023-05-19 10:43:35 +0300

    added colors to product card

--- JS/10-mini-shop/pages/catalog.js
+++ JS/10-mini-shop/pages/catalog.js
@@ -4,8 +4,17 @@ let [...allProducts] = dataBase.products;
 
 const showAllProducts = (data) => {
   data.forEach((item, idx) => {
-    const { brand, description, id, images, price, rating, title, thumbnail } =
-      item;
+    const {
+      brand,
+      description,
+      id,
+      images,
+      colors,
+      price,
+      rating,
+      title,
+      thumbnail,
+    } = item;
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
     productCard.innerHTML = `
@@ -13,9 +22,25 @@ const showAllProducts = (data) => {
       <h4 class="product-card__title">${title}</h4>
       <div class="product-card__grade">${Math.floor(rating)}</div>
       <p class="product-card__price">As low as <span class="product-card__cost">$${price}</span></p>
+      <div class="product-card__colors"></div>
       <button class="product-card__add">ADD TO CART</button>
     `;
+
     catalog.appendChild(productCard);
+
+    const productColors = document.createElement('div');
+    productColors.classList.add('product-card__colors');
+    colors.forEach((color) => {
+      const colorItem = document.createElement('div');
+      colorItem.classList.add('product-card__colors-item');
+      colorItem.style.background = `${color}`;
+      const [...productColors] = document.querySelectorAll(
+        '.product-card__colors'
+      );
+      productColors.forEach((item) => {
+        item.insertAdjacentElement('beforeend', colorItem);
+      });
+    });
   });
 };
 showAllProducts(allProducts);
