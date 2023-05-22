a67da94b5fc4616895c4f68a63bd90958858ee84
dryabtsun <riabtsun@i.ua>
2023-05-22 16:42:49 +0300

    added pagination

--- JS/10-mini-shop/pages/catalog.js
+++ JS/10-mini-shop/pages/catalog.js
@@ -3,59 +3,11 @@ const catalog = document.querySelector('.catalog');
 const catalogSearch = document.querySelector('.filterSearch-input');
 let [...allProducts] = dataBase.products;
 
-let currentPage = 1;
-let rows = 10;
-const showAllProducts = async (data, rowPerPage, page) => {
-  catalog.innerHTML = '';
-  page--;
+const renderCatalog = () => {
+  let currentPage = 1;
+  let rows = 10;
 
-  const start = rowPerPage * page;
-  const end = start + rowPerPage;
-
-  const paginatedData = data.slice(start, end);
-
-  function displayPagination(data, rowPerPage) {
-    const paginationEl = document.querySelector('.pagination');
-    const pagesCount = Math.ceil(data.length / rowPerPage);
-    const ulEl = document.createElement('ul');
-    ulEl.classList.add('pagination__list');
-    for (let i = 0; i < pagesCount; i++) {
-      const liEl = displayPaginationBtn(i + 1);
-      ulEl.append(liEl);
-    }
-    paginationEl.append(ulEl);
-  }
-
-  function displayPaginationBtn(page) {
-    const liEl = document.createElement('li');
-    liEl.classList.add('pagination__item');
-    liEl.innerText = page;
-
-    currentPage === page && liEl.classList.add('pagination__item--active');
-
-    liEl.addEventListener('click', () => {
-      currentPage = page;
-      showAllProducts(data, rows, currentPage);
-      let currentItemLi = document.querySelector('.pagination__item--active');
-      currentItemLi.classList.remove('pagination__item--active');
-      liEl.classList.add('pagination__item--active');
-    });
-    return liEl;
-  }
-  displayPagination(data, rowPerPage);
-
-  await paginatedData.forEach((item, idx) => {
-    const {
-      brand,
-      description,
-      id,
-      images,
-      colors,
-      price,
-      rating,
-      title,
-      thumbnail,
-    } = item;
+  const renderCards = ({ colors, price, rating, title, thumbnail }) => {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
     productCard.innerHTML = `
@@ -82,17 +34,71 @@ const showAllProducts = async (data, rowPerPage, page) => {
         item.insertAdjacentElement('beforeend', colorItem);
       });
     });
+  };
+  const showFilterProducts = (data) => {
+    catalog.innerHTML = '';
+
+    data.forEach((item, idx) => {
+      renderCards(item);
+    });
+  };
+  const showAllProducts = (data, rowPerPage, page) => {
+    catalog.innerHTML = '';
+
+    page--;
+
+    const start = rowPerPage * page;
+    const end = start + rowPerPage;
+
+    const paginatedData = data.slice(start, end);
+
+    paginatedData.forEach((item, idx) => {
+      renderCards(item);
+    });
+  };
+
+  function displayPagination(data, rowPerPage) {
+    const paginationEl = document.querySelector('.pagination');
+    const pagesCount = Math.ceil(data.length / rowPerPage);
+
+    const ulEl = document.createElement('ul');
+    ulEl.classList.add('pagination__list');
+    for (let i = 0; i < pagesCount; i++) {
+      const liEl = displayPaginationBtn(i + 1);
+      ulEl.append(liEl);
+    }
+    paginationEl.append(ulEl);
+  }
+
+  function displayPaginationBtn(page) {
+    const liEl = document.createElement('li');
+    liEl.classList.add('pagination__item');
+    liEl.innerText = page;
+
+    currentPage === page && liEl.classList.add('pagination__item--active');
+
+    liEl.addEventListener('click', () => {
+      currentPage = page;
+      showAllProducts(allProducts, rows, currentPage);
+      let currentItemLi = document.querySelector('.pagination__item--active');
+      currentItemLi.classList.remove('pagination__item--active');
+      liEl.classList.add('pagination__item--active');
+    });
+    return liEl;
+  }
+
+  displayPagination(allProducts, rows);
+  catalogSearch.addEventListener('keyup', (e) => {
+    catalog.innerHTML = ``;
+    let sortedProducts = allProducts.filter((product) => {
+      return product.title.toUpperCase().includes(e.target.value.toUpperCase());
+    });
+    showFilterProducts(sortedProducts);
   });
+
+  if (catalogSearch.value === '') {
+    showAllProducts(allProducts, rows, 1);
+  }
 };
 
-catalogSearch.addEventListener('keyup', (e) => {
-  catalog.innerHTML = ``;
-  let sortedProducts = allProducts.filter((product) => {
-    return product.title.toUpperCase().includes(e.target.value.toUpperCase());
-  });
-  showAllProducts(sortedProducts, rows, currentPage);
-});
-
-if (catalogSearch.value === '') {
-  showAllProducts(allProducts, rows, currentPage);
-}
+renderCatalog();
